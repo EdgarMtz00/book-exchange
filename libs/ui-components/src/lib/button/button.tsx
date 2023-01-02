90d212e3944517333312a27df4e529cffdcb5abe
EdgarMtz00 <edgar.agustin.martinez@gmail.com>
2023-01-01 22:25:45 -0600

    Optimize button styles

--- libs/ui-components/src/lib/button/button.tsx
+++ libs/ui-components/src/lib/button/button.tsx
@@ -16,37 +16,44 @@ export function Button(
     text,
     onPress
   }: ButtonProps) {
-  const styles  = StyleSheet.create({
-    buttonStyle: {
-      backgroundColor: buttonColor,
-      borderWidth: 0,
-      height: 40,
-      alignItems: 'center',
-      marginLeft: 35,
-      marginRight: 35,
-      marginTop: 20,
-      marginBottom: 25,
-      shadowColor: 'rgb(0,0,0)', // IOS
-      shadowOffset: { height: 5, width: 5 }, // IOS
-      shadowOpacity: 1, // IOS
-      shadowRadius: 0, //IOS
-      elevation: 4, // Android
-    },
-    buttonTextStyle: {
-      color: textColor,
-      paddingVertical: 10,
-      fontSize: 16,
-    }
-  })
+
+  const buttonColorStyle = {
+    backgroundColor: buttonColor,
+  }
+
+  const textColorStyle = {
+    color: textColor,
+  }
 
   return (
     <TouchableOpacity
-      style={styles.buttonStyle}
+      style={[styles.buttonStyle, buttonColorStyle]}
       activeOpacity={0.5}
       onPress={onPress}>
-      <Text style={styles.buttonTextStyle}>{text}</Text>
+      <Text style={[styles.textStyle, textColorStyle]}>{text}</Text>
     </TouchableOpacity>
   );
 }
+
+const styles  = StyleSheet.create({
+  buttonStyle: {
+    borderWidth: 0,
+    height: 40,
+    alignItems: 'center',
+    marginLeft: 35,
+    marginRight: 35,
+    marginTop: 20,
+    marginBottom: 25,
+    shadowColor: 'rgb(0,0,0)', // IOS
+    shadowOffset: { height: 5, width: 5 }, // IOS
+    shadowOpacity: 1, // IOS
+    shadowRadius: 0, //IOS
+    elevation: 4, // Android
+  },
+  textStyle: {
+    paddingVertical: 10,
+    fontSize: 16,
+  }
+})
 
 export default Button;
